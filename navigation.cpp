7ff1a0d60f42de78d2d75ced21f7cf1762dd34ec
Adam Pavlisin <lostmines.eu@gmail.com>
2017-12-02 17:05:39 +0000

    Update navigation.cpp

--- navigation.cpp
+++ navigation.cpp
@@ -33,9 +33,7 @@ double Navigation::get_acceleration()
 double Navigation::get_velocity()
 {
   /// TASK 8 (PERSON A) ///////////////////////////////////////////////////////
-  double a = this->get_acceleration();
-  this->velocity += a*DT; //integrate
-  return this->velocity;  
+  
   /// END OF TASK 8A ///////////////////////////////////////////////////////////
 }
 
